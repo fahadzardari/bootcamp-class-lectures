a978bd0cbc8052392a32d44b5603d6a487cadb0c
fahad <fahadzardari111@gmail.com>
2023-06-23 18:25:28 +0500

    lec completed

--- lecture 4/script.js
+++ lecture 4/script.js
@@ -1,6 +1,17 @@
-let name = 123;
-const name2 = "zulfiqar";
-console.log(name)
-console.log(name2)
-name = "Ali";
-console.log(name)
+// let obj = "my object";
+// let student1 = {
+//         name: "fahd",
+//         year: 12,
+//         city: "hyderabad"
+// }
+// alert(student1.year)
+
+// let student1 = "zulfiqar";
+// let student2 = "fahad";
+// let students = ["fhad" , "zulfiqar" , "ali" , "zohaib"];
+// document.getElementById("heading").innerText = "hi changed"
+
+function changeText(){
+        document.getElementById("heading").innerText = "hi changed"
+
+}
